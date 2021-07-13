33a250cb3cc0699fbaae742fb82796c12ac556c3
rafael.gonzalez <rafael.gonzalez@okmobilitygroup.com>
2021-07-13 11:06:38 +0200

    add

---
+++ src/library/useInput.ts
@@ -0,0 +1,11 @@
+import {useCallback, useState} from "react";
+
+export const useInput = (value: string) => {
+    const [state, setState] = useState(value);
+
+    const onChange = useCallback((evt) => {
+        setState(evt.target.value);
+    }, []);
+
+    return [state, onChange]
+}
